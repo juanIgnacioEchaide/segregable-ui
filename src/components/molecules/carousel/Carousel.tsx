68246268a606c600b2d3cc0582aa9208b78d9c12
j.echaide <jechaide@makingsense.com>
2022-09-11 01:12:09 -0300

    layout entities updated

--- src/components/molecules/carousel/Carousel.tsx
+++ src/components/molecules/carousel/Carousel.tsx
@@ -1,6 +1,7 @@
 import { ArrowLeftIcon, ArrowRightIcon } from "@chakra-ui/icons";
 import { IconButton } from "@chakra-ui/react";
 import { motion } from "framer-motion";
+import { VIEW } from "../../../common/constants/uri";
 
 const ArrowsContainer = ({ children }: any) => {
   return (
@@ -55,7 +56,7 @@ const DisplayedItem = ({ displayedData }: any) => {
   return <div>{displayedData}</div>;
 };
 
-const CarouselGrid = ({ pageNum, displayedData }: any) => {
+const NavigationGrid = ({ pageNum, displayedData }: any) => {
   return (
     <div
       style={{
@@ -101,4 +102,99 @@ const CarouselGrid = ({ pageNum, displayedData }: any) => {
     </div>
   );
 };
-export { CarouselGrid };
+
+const DetailsDisplayGrid = ({ view }: any) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+        padding: "30px",
+        fontSize: "30px",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+          alignItems: "center",
+          padding: "30px",
+          fontSize: "30px",
+          width: "30%",
+          height: "100%",
+        }}
+      >
+        DETAILS
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "left",
+          alignItems: "center",
+          padding: "30px",
+          fontSize: "30px",
+          width: "70%",
+          height: "100%",
+        }}
+      >
+        <DetailContentsByView view={view} data={null} />
+      </div>
+    </div>
+  );
+};
+
+const DetailContentsByView = ({ view, data }: any): JSX.Element => {
+  switch (view) {
+    case VIEW.FILMS:
+      return <FilmsDetailsContent data={data} />;
+    case VIEW.STARSHIP:
+      return <StarshipDetailsContent data={data} />;
+    case VIEW.SPECIES:
+      return <SpeciesDetailsContent data={data} />;
+    case VIEW.PLANETS:
+      return <PlanetsDetailsContent data={data} />;
+    case VIEW.VEHICLES:
+      return <VehiclesDetailsContent data={data} />;
+    case VIEW.PEOPLE:
+      return <PeopleDetailsContent data={data} />;
+    default:
+      return <PeopleDetailsContent data={data} />;
+  }
+};
+
+const FilmsDetailsContent = ({ data }: any) => {
+  return <div>FilmsDetailsContent</div>;
+};
+
+const PlanetsDetailsContent = ({ data }: any) => {
+  return <div>PlanetsDetailsContent</div>;
+};
+const PeopleDetailsContent = ({ data }: any) => {
+  return <div>PeopleDetailsContent</div>;
+};
+const VehiclesDetailsContent = ({ data }: any) => {
+  return <div>PeopleDetailsContent</div>;
+};
+const SpeciesDetailsContent = ({ data }: any) => {
+  return <div>PeopleDetailsContent</div>;
+};
+const StarshipDetailsContent = ({ data }: any) => {
+  return <div>StarshipDetailsContent</div>;
+};
+
+export {
+  NavigationGrid,
+  DetailsDisplayGrid,
+  FilmsDetailsContent,
+  PlanetsDetailsContent,
+  PeopleDetailsContent,
+  VehiclesDetailsContent,
+  SpeciesDetailsContent,
+  StarshipDetailsContent,
+};
--- src/components/templates/PageTemplate.tsx
+++ src/components/templates/PageTemplate.tsx
@@ -1,30 +1,32 @@
 import { Grid, GridItem } from "@chakra-ui/react";
-import { CarouselGrid } from "../molecules/carousel/Carousel";
+import {
+  NavigationGrid,
+  DetailsDisplayGrid,
+} from "../molecules/carousel/Carousel";
 
 const PageTemplate = (): JSX.Element => {
   return (
     <Grid
       h="100%"
       paddingTop="2px"
-      templateRows="repeat(2, 1fr)"
+      templateRows="repeat(4, 1fr)"
       templateColumns="repeat(5, 1fr)"
       gap={2}
     >
       <GridItem
         colSpan={5}
-        rowSpan={1}
+        rowSpan={2}
         bg="tomato"
         placeItems={"center"}
-        children={<CarouselGrid />}
+        children={<NavigationGrid />}
       />
       <GridItem
         colSpan={5}
-        rowSpan={1}
+        rowSpan={6}
         bg="tomato"
         placeItems={"center"}
-        children={"detalle"}
+        children={<DetailsDisplayGrid />}
       />
-      <GridItem colSpan={5} bg="papayawhip" />
     </Grid>
   );
 };
