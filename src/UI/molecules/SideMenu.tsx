dd1524476e23613dfebde2991e6f1e744f1d9b16
j.echaide <jechaide@makingsense.com>
2022-11-05 16:38:47 -0300

    update bug fix draft

--- src/UI/molecules/SideMenu.tsx
+++ src/UI/molecules/SideMenu.tsx
@@ -1,14 +1,19 @@
-import { RoutesStack } from '../../AppRouter'
-import { SideContainer, SideLink } from '../atoms/SideMenu'
+import { RoutesStack } from "../../AppRouter";
+import { SideContainer, SideLink } from "../atoms/SideMenu";
 
-const SideMenu = () => (
+const SideMenu = ({ setView }: any) => (
   <SideContainer>
     {RoutesStack?.map((i) => (
-      <SideLink style={{ paddingTop: '5vh' }} to={i.path}>
+      <SideLink
+        style={{ paddingTop: "5vh" }}
+        key={i.name}
+        onClick={() => setView(i.name)}
+        to={i.path}
+      >
         {i.name}
       </SideLink>
     ))}
   </SideContainer>
-)
+);
 
-export { SideMenu }
+export { SideMenu };
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,24 @@
-import { useContext, useEffect, useState } from 'react'
-import { Grid } from '../../UI/molecules/Grid'
-import { SideMenu } from '../../UI/molecules/SideMenu'
-import { QueryContext } from '../../context/query/QueryProvider'
+import { useContext, useEffect, useState } from "react";
+import { Grid } from "../../UI/molecules/Grid";
+import { SideMenu } from "../../UI/molecules/SideMenu";
+import { QueryContext } from "../../context/query/QueryProvider";
+import { VIEW } from "../../common/constants/uri";
+import { BaseActions } from "../../context/query/BaseActions";
 
 const Home = (): JSX.Element => {
-  const { state, dispatch } = useContext(QueryContext)
-
+  const { state, dispatch } = useContext(QueryContext);
+  const setView = (viewName: VIEW) => {
+    BaseActions.SetView(viewName);
+  };
   return (
     <Grid
       nav={<div>swapi</div>}
       main={undefined}
       details={<p>DETALLE</p>}
-      side={<SideMenu />}
+      side={<SideMenu setView={setView} />}
       footer={undefined}
     />
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
