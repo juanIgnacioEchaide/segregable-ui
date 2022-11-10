a787c52d8748f49ce95b3535b35d1a5ffd9e2a8b
jechaide@technisys.com <jechaide@makingsense.com>
2022-11-10 10:46:04 -0300

    succesfully rendering items

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,42 +1,54 @@
-import { useCallback, useContext, useEffect, useState } from "react";
-import { Grid } from "../../UI/molecules/Grid";
-import { SideMenu } from "../../UI/molecules/SideMenu";
-import { QueryContext } from "../../context/query/QueryProvider";
-import { VIEW } from "../../common/constants/uri";
-import { BaseActions } from "../../context/query/BaseActions";
-import { getViewState } from "../../common/utils/helpers";
+import { useCallback, useContext, useEffect, useState } from 'react'
+import { Grid } from '../../UI/molecules/Grid'
+import { SideMenu } from '../../UI/molecules/SideMenu'
+import { QueryContext } from '../../context/query/QueryProvider'
+import { VIEW } from '../../common/constants/uri'
+import { BaseActions } from '../../context/query/BaseActions'
+import { getViewState } from '../../common/utils/helpers'
 
 const Home = (): JSX.Element => {
-  const { state, dispatch } = useContext(QueryContext);
-  const [title, setTitle] = useState<any>();
-  const [items, setItems] = useState<any[]>([]);
+  const { state, dispatch } = useContext(QueryContext)
+  const [title, setTitle] = useState<any>()
+  const [items, setItems] = useState<any[]>([])
 
   const setView = (viewName: VIEW) => {
-    dispatch(BaseActions.SetView(viewName));
-  };
+    dispatch(BaseActions.SetView(viewName))
+  }
 
-  const getItemsByView = () => getViewState(state);
+  const getItemsByView = () => getViewState(state)
 
   useEffect(() => {
-    if (state?.view) setTitle(state.view);
-  }, [state]);
+    if (state?.view) setTitle(state.view)
+  }, [state])
 
   useEffect(() => {
     if (state) {
-      setItems(getItemsByView());
+      setItems(getItemsByView())
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [state]);
+  }, [state])
+
+  useEffect(() => {
+    if (items?.length) {
+      console.log(items)
+    }
+  }, [items])
 
   return (
     <Grid
       nav={<div>swapi</div>}
-      main={undefined}
+      main={
+        <div>
+          {items?.map((i) => (
+            <p key={i.title}>{i.title}</p>
+          ))}
+        </div>
+      }
       details={<p>{title}</p>}
       side={<SideMenu setView={setView} />}
       footer={undefined}
     />
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
